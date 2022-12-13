aedbc145199fffb9826880f921f4102a984f9565
jasminealbert <116348107+jasminealbert@users.noreply.github.com>
2022-12-13 14:00:56 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,29 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# streamlit.write('The user entered ', fruit_choice)
 
-#new section for api response
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information')
+   else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalised)
+except URLError as e:
+  streamlit.error()
+
+# #new section for api response
+# #import requests
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
 
-#normalises json output 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output as table
-streamlit.dataframe(fruityvice_normalized)
+# #normalises json output 
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# # output as table
+# streamlit.dataframe(fruityvice_normalized)
 
 #import snowflake.connector
 streamlit.stop()
